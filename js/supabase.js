89d8f5e93816f8183306e14989e7fa911980b0ec
sunho <niceguy3051@naver.com>
2025-07-26 03:10:54 +0900

    Supabase 설정 수정 - 로그인 문제 해결

--- js/supabase.js
+++ js/supabase.js
@@ -1,7 +1,6 @@
 // Supabase 연결 설정
-// TODO: Supabase 프로젝트 URL과 API 키를 입력하세요
-const SUPABASE_URL = 'YOUR_SUPABASE_URL';
-const SUPABASE_ANON_KEY = 'YOUR_SUPABASE_ANON_KEY';
+const SUPABASE_URL = 'https://jykkpfrpnpkycqyokqnm.supabase.co';
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imp5a2twZnJwbnBreWNxeW9rcW5tIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTI3MTY1NjgsImV4cCI6MjA2ODI5MjU2OH0.vMXLe-ccOQXuH2I6M-9WIYJcxoCMQygh5ldBGdd3jzk';
 
 // Supabase 클라이언트 생성
 const supabase = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
